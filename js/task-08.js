19db595ff4c58a35304f53a0a2e9ea3f2ecdf0c9
Na7aly <tomac.natalia95@gmail.com>
2024-01-07 17:13:18 +0200

    resolve the task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const loginForm = document.querySelector('.login-form');
 
+// Adăugați un event listener pentru evenimentul "submit"
+loginForm.addEventListener('submit', function (event) {
+  // Opriți comportamentul implicit al formularului (evitând reîncărcarea paginii)
+  event.preventDefault();
+
+  // Obțineți elementele formularului
+  const emailInput = loginForm.elements['email'];
+  const passwordInput = loginForm.elements['password'];
+
+  // Verificați dacă toate câmpurile sunt completate
+  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    // Afișați un mesaj de alertă dacă există câmpuri goale
+    alert('Please fill in all fields.');
+  } else {
+    // Creați un obiect cu datele introduse
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    // Afișați obiectul cu datele introduse în consolă
+    console.log(formData);
+
+    // Ștergeți valorile câmpurilor din formular
+    loginForm.reset();
+  }
+});
